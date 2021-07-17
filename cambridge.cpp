82f3a19c8764a79f9c84ce02b0d3b55aab16c1bd
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2021-07-17 13:02:00 +0530

    Add files via upload

---
+++ cambridge.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char word[] ="CAMBRIDGE";
+    int counter = 0;
+
+    char input_word[10000] = {'\0'};
+
+    cin>>input_word;
+
+    for (int i = 0; i < 10000; i++)
+    {
+        if (input_word[i])
+        {
+            /* code */
+        }
+        
+    }
+    
+    
+    return 0;
+}
